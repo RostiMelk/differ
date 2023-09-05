4e324d2154c35ac0e46b66c484431c39518b9cf9
RostiMelk <hello@rosti.no>
2023-09-05 20:10:10 +0200

    feat: media query favicon

--- src/components/DynamicHead.tsx
+++ src/components/DynamicHead.tsx
@@ -1,23 +1,42 @@
 import Head from "next/head";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+
+type Favicon = "default" | "diff" | "equal";
 
 interface HeadProps {
   titleExtra?: string;
-  favicon?: "default" | "diff" | "equal";
+  favicon?: Favicon;
 }
 
-const faviconMap = {
-  default: "/shapes.svg",
-  diff: "/diff.svg",
-  equal: "/check.svg",
+const faviconMap: Record<Favicon, Record<"light" | "dark", string>> = {
+  default: {
+    light: "/shapes.svg",
+    dark: "/shapes-dark.svg",
+  },
+  diff: {
+    light: "/diff.svg",
+    dark: "/diff-dark.svg",
+  },
+  equal: {
+    light: "/check.svg",
+    dark: "/check-dark.svg",
+  },
 };
 
 export const DynamicHead = ({ titleExtra, favicon }: HeadProps) => {
+  const isDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
   const title = titleExtra ? `Differ | ${titleExtra}` : "Differ";
+  const faviconKey = favicon ?? "default";
+  const faviconSrc = isDarkMode
+    ? faviconMap[faviconKey].dark
+    : faviconMap[faviconKey].light;
+
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content="A tool for comparing two web pages." />
-      <link rel="icon" href={faviconMap[favicon ?? "default"]} />
+      <link rel="icon" href={faviconSrc} />
     </Head>
   );
 };
